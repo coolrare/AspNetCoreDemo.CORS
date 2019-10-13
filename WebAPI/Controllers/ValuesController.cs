da992e4ec7ff0284189684fde67140c40ae6ee80
Will <doggy.huang@gmail.com>
2019-10-13 13:12:48 +0800

    加入 WebAPI 與 WebClient 跨來源 AJAX 呼叫範例專案。

---
+++ WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "GET", "value1", "value2" };
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult<IEnumerable<string>> Post()
+        {
+            return new string[] { "POST", "value1", "value2" };
+        }
+
+        // PUT api/values
+        [HttpPut]
+        public ActionResult<IEnumerable<string>> Put()
+        {
+            return new string[] { "PUT", "value1", "value2" };
+        }
+
+        // DELETE api/values
+        [HttpDelete]
+        public ActionResult<IEnumerable<string>> Delete()
+        {
+            return new string[] { "DELETE", "value1", "value2" };
+        }
+    }
+}
---
+++ WebClient/Startup.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebClient
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            DefaultFilesOptions dfOptions = new DefaultFilesOptions();
+            dfOptions.DefaultFileNames.Clear();
+            dfOptions.DefaultFileNames.Add("index.html");
+            app.UseDefaultFiles(dfOptions);
+        
+            app.UseStaticFiles();
+        }
+    }
+}
